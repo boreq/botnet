7da92bf700843bd2434767b81c94e516e0e3501e
boreq <boreq@sourcedrops.com>
2015-05-19 22:58:00 +0200

    Removed imports

--- botnet/modules/__init__.py
+++ botnet/modules/__init__.py
@@ -1,4 +1,3 @@
 from .base import BaseIdleModule, BaseModule
 from .baseresponder import BaseResponder
-from .mixins import ConfigMixin, MessageDispatcherMixin
 from .utils import parse_command, get_module, reload_module
