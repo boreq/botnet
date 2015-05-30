b31250e139229f633c1fe3fd4002f8dbfe362c6d
boreq <boreq@sourcedrops.com>
2015-05-30 18:34:22 +0200

    Imported stuff useful when implementing external modules into the main package.

--- botnet/__init__.py
+++ botnet/__init__.py
@@ -1,5 +1,14 @@
+# Directly related to the implementation of the bot
+from .codes import Code
+from .helpers import is_channel_name
+from .logging import get_logger
+from .message import Message
 from .modules import BaseModule, BaseResponder, ConfigMixin, \
     BaseMessageDispatcherMixin, StandardMessageDispatcherMixin, \
     AdminMessageDispatcherMixin
+from .signals import message_in, admin_message_in, message_out, on_exception, \
+    module_load, module_unload, module_loaded, module_unloaded, config_reload, \
+    config_reloaded, config_changed
 
+# Additional utilities for modules
 from .modules import lib
