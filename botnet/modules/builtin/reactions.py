38ad5a113adab65ef2a0732584368f1d8fb7bbb3
boreq <boreq@sourcedrops.com>
2016-07-31 14:16:48 +0200

    Added reactions module

---
+++ botnet/modules/builtin/reactions.py
@@ -0,0 +1,85 @@
+import random
+from .. import BaseResponder
+from ..lib import parse_command
+
+
+cutelist = [
+    '✿◕ ‿ ◕✿',
+    '❀◕ ‿ ◕❀',
+    '(✿◠‿◠)',
+    '(◕‿◕✿) ',
+    '( ｡◕‿◕｡)',
+    '(◡‿◡✿)',
+    '⊂◉‿◉つ ❤',
+    '{ ◕ ◡ ◕}',
+    '( ´・‿-) ~ ♥',
+    '(っ⌒‿⌒)っ~ ♥',
+    'ʕ´•ᴥ•`ʔσ”',
+    '(･Θ･) caw',
+    '(=^･ω･^)y＝',
+    'ヽ(=^･ω･^=)丿',
+    '~(=^･ω･^)ヾ(^^ )',
+    '| (•□•) | (❍ᴥ❍ʋ)',
+    'ϞϞ(๑⚈ ․̫ ⚈๑)∩',
+    'ヾ(･ω･*)ﾉ',
+    '▽・ω・▽ woof~',
+    '(◎｀・ω・´)人(´・ω・｀*)',
+    '(*´・ω・)ノ(-ω-｀*)',
+    '(❁´ω`❁)',
+    '(＊◕ᴗ◕＊)',
+    '{´◕ ◡ ◕｀}',
+    '₍•͈ᴗ•͈₎',
+    '(˘･ᴗ･˘)',
+    '(ɔ ˘⌣˘)˘⌣˘ c)',
+    '(⊃｡•́‿•̀｡)⊃',
+    '(´ε｀ )♡',
+    '(◦˘ З(◦’ںˉ◦)♡',
+    '( ＾◡＾)っ~ ❤ Leper',
+    '╰(　´◔　ω　◔ `)╯',
+    '(*･ω･)',
+    '(∗•ω•∗)',
+    '( ◐ω◐ )'
+    '(✿◠‿◠)っ~ ♥ {target}',
+    '⊂◉‿◉つ ❤ {target}',
+    '( ´・‿-) ~ ♥ {target}',
+    '(っ⌒‿⌒)っ~ ♥ {target}',
+    'ʕ´•ᴥ•`ʔσ” BEARHUG {target}',
+    '(⊃｡•́‿•̀｡)⊃ U GONNA GET HUGGED {target}',
+    '( ＾◡＾)っ~ ❤ {target}',
+    '{target} (´ε｀ )♡',
+    '{sender} ~(=^･ω･^)ヾ(^^ ) {target}',
+    '{sender} (◎｀・ω・´)人(´・ω・｀*) {target}',
+    '{sender} (*´・ω・)ノ(-ω-｀*) {target}',
+    '{sender} (ɔ ˘⌣˘)˘⌣˘ c) {target}',
+    '{sender} (◦˘ З(◦’ںˉ◦)♡ {target}',
+]
+
+
+magiclist = [
+    '(つ˵•́ω•̀˵)つ━☆ﾟ.*･｡ﾟ҉̛ {target}',
+    '(つ˵•́ω•̀˵)つ━☆✿✿✿✿✿✿ {target}',
+    '╰( ´・ω・)つ──☆ﾟ.*･｡ﾟ҉̛ {target}',
+    '╰( ´・ω・)つ──☆✿✿✿✿✿✿ {target}',
+    '(○´･∀･)o<･。:*ﾟ;+． {target}',
+]
+
+
+class Reactions(BaseResponder):
+    """Defines several commands related to emotional reactions.""" 
+
+    @parse_command([('target', '*')])
+    def command_cute(self, msg, args):
+        self.send_from_list(msg, args, cutelist)
+
+    @parse_command([('target', '*')])
+    def command_magic(self, msg, args):
+        self.send_from_list(msg, args, magiclist)
+
+    def send_from_list(self, msg, args, reactions_list):
+        target = ' '.join(args.target)
+        response = random.choice(reactions_list).format(sender=msg.nickname,
+                                                        target=target)
+        self.respond(msg, response)
+
+
+mod = Reactions
