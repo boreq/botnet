07390d2986ffb09dbb5d92ba1005c0961a8f07b8
boreq <boreq@sourcedrops.com>
2016-07-31 13:01:18 +0200

    Quotes: added auto discovery

--- botnet/modules/builtin/quotes.py
+++ botnet/modules/builtin/quotes.py
@@ -1,3 +1,4 @@
+import os
 import random
 from ...signals import on_exception
 from .. import BaseResponder
@@ -9,14 +10,22 @@ def random_line(filename):
 
 
 class Quotes(BaseResponder):
-    """Sends random lines from the files defined in config.
+    """Sends random lines from the files defined in config. This module will
+    discover the files automatically by name if a directory containing them
+    is included in the config.
 
     Example module config:
 
         "botnet": {
             "quotes": {
-                "command_name1": "filepath1",
-                "command_name2": "filepath2"
+                "directories": [
+                    "path/to/directory/with/quote/files1",
+                    "path/to/directory/with/quote/files2"
+                ],
+                "files": {
+                    "command_name1": "filepath1",
+                    "command_name2": "filepath2"
+                }
             }
         }
 
@@ -37,13 +46,27 @@ class Quotes(BaseResponder):
     def handle_privmsg(self, msg):
         if self.is_command(msg):
             key = self.get_command_name(msg)
-            filename = self.config_get(key, None)
+
+            # Directories
+            for directory in self.config_get('directories', []):
+                for root, dirs, files in os.walk(directory):
+                    for filename in files:
+                        if filename == key:
+                            path = os.path.join(root, filename)
+                            self.send_random_line(msg, path)
+                            return
+
+            # Files
+            filename = self.config_get('files.%s' % key, None)
             if filename is not None:
-                try:
-                    line = random_line(filename)
-                    self.respond(msg, line)
-                except FileNotFoundError as e:
-                    on_exception.send(self, e=e)
+                self.send_random_line(msg, filename)
+
+    def send_random_line(self, msg, filepath):
+        try:
+            line = random_line(filepath)
+            self.respond(msg, line)
+        except FileNotFoundError as e:
+            on_exception.send(self, e=e)
 
 
 mod = Quotes
