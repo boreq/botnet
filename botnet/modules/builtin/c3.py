8c27d381a3cea389b88bb5de89e722dd53e8958f
boreq <boreq@sourcedrops.com>
2018-01-03 02:07:23 +0100

    Added module c3

---
+++ botnet/modules/builtin/c3.py
@@ -0,0 +1,31 @@
+import datetime
+from .. import BaseResponder
+
+
+class C3(BaseResponder):
+    """Defines functions related to Chaos Communication Congress."""
+
+    FIRST_CONGRESS_YEAR = 1984
+    CONGRESS_MONTH = 12
+    CONGRESS_DAY = 27
+
+    def command_c3(self, msg):
+        """Displays the number of days remaining to the next congress.
+
+        Syntax: c3
+        """
+        now = datetime.datetime.utcnow()
+        congress = datetime.datetime(year=now.year,
+                                     month=self.CONGRESS_MONTH,
+                                     day=self.CONGRESS_DAY)
+        if congress < now:
+            congress = congress.replace(year = congress.year + 1)
+
+        number = congress.year - self.FIRST_CONGRESS_YEAR + 1
+        days = (congress - now).days
+
+        text = 'Time to {number}C3: {days} days'.format(number=number, days=days)
+        self.respond(msg, text)
+
+
+mod = C3
