8692a373a4ca5f39a979e4326edc335c959df0a9
user <email@email.com>
2018-05-20 11:07:40 +1200

    Fixed the bot

--- botnet/modules/lib/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-    Various utilities which are not a part of the actual module system
-    implementation but rather form a library which can be used by modules.
-"""
-from .cache import BaseCache, MemoryCache
-from .decorators import parse_command, catch_other
-from .network import get_url
