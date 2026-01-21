190079458e94df3c117da5cb5b075a6bf5ee7e94
boreq <boreq@sourcedrops.com>
2026-01-22 00:10:29 +0100

    Fix quotes module tests

--- tests/modules/builtin/test_quotes.py
+++ tests/modules/builtin/test_quotes.py
@@ -1,27 +1,25 @@
 import os
+from botnet.message import Message
 from botnet.config import Config
 from botnet.modules.builtin.quotes import Quotes
-from botnet.signals import message_in
 
 
-def test_quotes(msg_t, make_privmsg):
+def test_quotes(module_harness_factory, make_privmsg):
     dirname = os.path.dirname(os.path.realpath(__file__))
     filename = os.path.join(dirname, 'quotes')
 
-    q = Quotes(Config())
+    m = module_harness_factory.make(Quotes, Config())
+
     msg = make_privmsg('.lotr')
+    m.receive_message_in(msg)
+    m.expect_message_out_signals([])
 
-    message_in.send(None, msg=msg)
-    assert not msg_t.msg
-
-    q.config_set('files.lotr', filename)
-    message_in.send(None, msg=msg)
-    assert msg_t.msg
-
-
-def test_quotes_gone(make_privmsg):
-    q = Quotes(Config())
-    msg = make_privmsg('.gone')
-
-    q.config_set('files.gone', 'gone')
-    message_in.send(None, msg=msg)
+    m.module.config_set('files.lotr', filename)
+    m.receive_message_in(msg)
+    m.expect_message_out_signals(
+        [
+            {
+                'msg': Message.new_from_string('PRIVMSG #channel :Three Rings for the Elven-kings under the sky')
+            }
+        ]
+    )
