7c36600bad587fa84fa71e2d7b6f915829541d69
boreq <boreq@sourcedrops.com>
2026-01-24 15:40:57 +0100

    Hallucinate corect type annotations for decorators

--- src/botnet/modules/decorators.py
+++ src/botnet/modules/decorators.py
@@ -1,6 +1,6 @@
 import argparse
 import inspect
-from typing import Protocol, Any, Callable, TypeVar
+from typing import Any, Callable, cast
 from botnet.message import IncomingPrivateMessage
 from .base import AuthContext
 from functools import wraps
@@ -10,36 +10,35 @@ _ATTR_COMMAND_NAME = '_command_name'
 _ATTR_PREDICATES = '_predicates'
 
 
-T = TypeVar("T", contravariant=True)
+class Args(dict[str, list[str]]):
+
+    def __init__(self, namespace: argparse.Namespace) -> None:
+        super().__init__()
+        for name in dir(namespace):
+            if not name.startswith('_'):
+                value = getattr(namespace, name)
+                if not callable(value):
+                    self[name] = value
 
 
-class CommandHandler(Protocol[T]):
-    def __call__(self, instance: T, msg: IncomingPrivateMessage, auth: AuthContext) -> None:
-        ...
+CommandHandler = Callable[[Any, IncomingPrivateMessage, AuthContext], None]
+CommandHandlerWithArgs = Callable[[Any, IncomingPrivateMessage, AuthContext, Args], None]
 
 
-class CommandHandlerWithArguments(Protocol[T]):
-    def __call__(self, instance: T, msg: IncomingPrivateMessage, auth: AuthContext, args: Args) -> None:
-        ...
+Predicate = Callable[[Any, IncomingPrivateMessage, AuthContext], bool]
 
 
-def command(name: str):
-    """Decorator which marks methods as commands. The wrapped method may
-    optionally have the following arguments:
-        - msg: Message
-        - auth: AuthContext
-
-    The arguments must have the same names if they are present. The messages
-    passed to those functions are always of kind PRIVMSG.
+def command(name: str) -> Callable[[CommandHandler], CommandHandler]:
+    """Decorator which marks methods as commands.
 
     Example:
 
         @command('hello')
-        def command_hello(self, msg: Message) -> None:
+        def command_hello(self, msg: IncomingPrivateMessage, auth: AuthContext) -> None:
             self.respond(msg, 'Hello world!')
 
         @command('admin_hello')
-        def command_admin_hello(self, msg: Message, auth: AuthContext) -> None:
+        def command_admin_hello(self, msg: IncomingPrivateMessage, auth: AuthContext) -> None:
             if 'admin' in auth.groups:
                 self.respond(msg, 'Hello world, admin!')
     """
@@ -50,22 +49,21 @@ def command(name: str):
     return decorator
 
 
-class Predicate(Protocol):
-    def __call__(self, module: Any, msg: IncomingPrivateMessage, auth: AuthContext) -> bool:
-        ...
-
-
 def predicates(predicates: list[Predicate]) -> Callable[[CommandHandler], CommandHandler]:
     """Decorator which adds predicates which must be fulfilled to launch this command.
 
     Example:
 
+        def admin_only_predicate(module: Any, msg: IncomingPrivateMessage, auth: AuthContext) -> bool:
+            return 'admin' in auth.groups
+
         @command('hello')
         @predicates([admin_only_predicate])
-        def command_admin_hello(self, msg: Message) -> None:
+        def command_admin_hello(self, msg: IncomingPrivateMessage, auth: AuthContext) -> None:
+            assert 'admin' in auth.groups
             self.respond(msg, 'Hello admin!')
     """
-    def decorator(f):
+    def decorator(f: CommandHandler) -> CommandHandler:
         all_predicates = getattr(f, _ATTR_PREDICATES, [])
         all_predicates.extend(predicates)
         setattr(f, _ATTR_PREDICATES, all_predicates)
@@ -83,35 +81,25 @@ def _any_group(groups: list[str]) -> Callable[[CommandHandler], CommandHandler]:
     return predicates([predicate])
 
 
-def only_admins():
+def only_admins() -> Callable[[CommandHandler], CommandHandler]:
     """Decorator which makes the command accessible only to admins.
 
     Example:
 
         @command('hello')
         @only_admins()
-        def command_admin_hello(self, msg: Message) -> None:
+        def command_admin_hello(self, msg: IncomingPrivateMessage, auth: AuthContext) -> None:
             self.respond(msg, 'Hello admin!')
     """
     return _any_group(['admin'])
 
 
-class Args(dict[str, list[str]]):
-
-    def __init__(self, namespace: argparse.Namespace) -> None:
-        for name in dir(namespace):
-            if not name.startswith('_'):
-                value = getattr(namespace, name)
-                if not callable(value):
-                    self[name] = value
-
-
-def parse_command(params):
+def parse_command(params) -> Callable[[CommandHandlerWithArgs], CommandHandler]:
     """Decorator which parses the last argument of PRIVMSG, which is the
     message itself, using argparse.
 
         @parse_command([('person', '1'), ('colors', '+')])
-        def command_colors(self, msg, auth, args):
+        def command_colors(self, msg: IncomingPrivateMessage, auth: AuthContext, args: Args) -> None:
             colors = ' '.join(args['colors'])
             self.respond(msg, '%s likes those colors: %s' % (args['person'][0], colors))
 
@@ -121,20 +109,20 @@ def parse_command(params):
     for name, nargs in params:
         parser.add_argument(name, nargs=nargs)
 
-    def decorator(f):
+    def decorator(f: CommandHandlerWithArgs) -> CommandHandler:
         sig = inspect.signature(f)
         if 'args' not in sig.parameters.keys():
             raise Exception('function signature is missing args')
         new_params = [p for name, p in sig.parameters.items() if name != 'args']
 
         @wraps(f)
-        def decorated_function(self: Any, msg: IncomingPrivateMessage, auth: AuthContext) -> None:
+        def decorated_function(self: Any, msg: IncomingPrivateMessage, auth: AuthContext, *a: Any, **kw: Any) -> None:
             try:
-                args = parser.parse_args(msg.text.s.split())
+                namespace = parser.parse_args(msg.text.s.split())
             except argparse.ArgumentError:
                 return
-            f(self, msg, auth, Args(args))
+            f(self, msg, auth, Args(namespace), *a, **kw)
 
         setattr(decorated_function, '__signature__', sig.replace(parameters=new_params))
-        return decorated_function
+        return cast(CommandHandler, decorated_function)
     return decorator
